d230d3f41059b0eb3394878fbe9ddd2ed1e2bd41
kobeyvines <12995.2020@students.ku.ac.ke>
2026-04-15 18:17:21 +0300

    feat: setup github pages ci/cd pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  // GitHub Pages routing configuration
+  site: 'https://Postcards-from-Nyumbani.github.io',
+  base: '/postcards-from-nyumbani',
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
