1e34bdcdab8a50726f62949ab7b6ded50b249ff5
kobeyvines <12995.2020@students.ku.ac.ke>
2026-04-15 19:30:09 +0300

    chore: remove base path for root github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  // The absolute domain (dashes)
+  // The absolute domain
   site: 'https://postcards-from-nyumbani.github.io',
   
-  // If building on GitHub, use the subfolder. If on your computer, use normal routing.
-  base: process.env.GITHUB_ACTIONS ? '/postcards_from_nyumbani' : undefined,
+  // Notice we deleted the 'base' line completely!
 
-  // THIS IS THE MISSING LINCHPIN: Forces GitHub to read folders correctly
+  // Keep this to ensure GitHub reads your folders correctly
   trailingSlash: 'always',
 
   integrations: [
